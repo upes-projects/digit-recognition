7140a6c0149867c2411f3e08db1f3c198a2b997e
Red-Fleet <anshul145263@gmail.com>
2018-09-19 01:23:29 +0530

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,69 @@
+#include"matrix.h"
+
+struct stack_node{
+	struct matrix * data;
+	struct stack_node * point;
+};
+
+struct stack{
+	long size;
+	struct stack_node *top;
+};
+
+struct stack * newStack(){
+	struct stack * sta = (struct stack *) malloc(sizeof(struct stack));
+	sta->size = 0;
+	sta->top = NULL;
+
+	return sta;
+}
+
+long countStackElement(struct stack *sta){
+	return sta->size;
+}
+
+void pushStackElement(struct stack *sta, struct matrix *mat){
+	sta->size++;
+	
+	struct stack_node * node = (struct stack_node*) malloc(sizeof(struct stack_node));
+	node->data = mat;
+
+	node->point = sta->top;
+
+	sta->top = node;
+}
+
+struct matrix * popStackElement(struct stack *sta){
+	struct matrix * mat = (sta->top)->data;
+	struct stack_node * temp = sta->top;
+	sta->top = temp->point;
+	free(temp);
+
+	return mat;
+}
+
+void popStackElementDeleteMatrix(struct stack *sta){
+	struct matrix * mat = popStackElement(sta); 
+	deleteMatrix(mat);
+
+}
+
+
+
+struct matrix * viewStackElement(struct stack *sta){
+	return (sta->top)->data;
+}
+
+
+void deleteStack(struct stack *sta){
+	while(sta->size){
+		popStackElementDeleteMatrix(sta);
+		sta->size--;
+	}
+
+	free(sta);
+}
+
+
+
+
